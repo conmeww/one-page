bf97b5384ae77a2cc87517e5b935929daf84037e
conmeww <73779105+conmeww@users.noreply.github.com>
2022-01-23 13:47:09 +0300

    commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(function () {
   $(window).on("load resize", function () {
     var w = $(window).width();
     var h = $(window).height();
-    var x = 992;
+    var x = 800;
     if (w >= x) {
       $("nav ul").css({ display: "flex", height: "auto" });
     } else {
